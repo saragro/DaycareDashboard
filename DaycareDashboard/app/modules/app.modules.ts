ffbf3d31a207a5bbb3c7a3004922992c60f551e4
saragro <sgrossma5@gmail.com>
2017-05-17 20:39:45 +0300

    babyLog model + service + json

--- DaycareDashboard/app/modules/app.modules.ts
+++ DaycareDashboard/app/modules/app.modules.ts
@@ -1,6 +1,6 @@
 ﻿import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { BabyLogComponent } from "../index";
+import { BabyLogComponent, ActivityLogService } from "../index";
 import { HttpModule } from "@angular/http";
 
 
@@ -8,7 +8,8 @@ import { HttpModule } from "@angular/http";
 @NgModule({
     imports: [BrowserModule, HttpModule],
     declarations: [BabyLogComponent],
-    bootstrap: [BabyLogComponent]
+    bootstrap: [BabyLogComponent],
+    providers: [ActivityLogService]
 })
 export class AppModule {
 }
---
+++ DaycareDashboard/app/services/log.service.ts
@@ -0,0 +1,35 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import { BabyLog } from "../index";
+
+@Injectable()
+export class ActivityLogService {
+
+    private logUrl: string = 'app/assets/activity-log.json';
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+
+    constructor(private http: Http) {
+
+    }
+
+    getActivityLogsForBaby(): Observable<BabyLog[]>{
+        return this.http.get(this.logUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    private extractData(response: Response): any {
+        let res = response.json();
+        return res;
+    }
+
+    private handleError(error: any) {
+        let msg = error.message ? error.message : 'Unknown Error';
+        console.error(msg);
+        return Observable.throw(msg);
+
+    }
+}
