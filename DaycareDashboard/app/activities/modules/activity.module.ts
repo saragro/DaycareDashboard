0e63f730f81394f6aaa73c82a9902aa903424139
Sara Grossman <sgrossma5@gmail.com>
2017-06-01 18:24:33 +0300

    add shared module

--- DaycareDashboard/app/activities/modules/activity.module.ts
+++ DaycareDashboard/app/activities/modules/activity.module.ts
@@ -1,11 +1,13 @@
 ﻿import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { AddActivityComponent, AddActivityService, ActivityPipe } from "../index";
+import { AddActivityComponent, AddActivityService } from "../index";
 import { HttpModule } from "@angular/http";
+import { SharedModule } from '../../shared/index';
+
 
 @NgModule({
-    imports: [CommonModule, HttpModule],
-    declarations: [AddActivityComponent, ActivityPipe],
+    imports: [CommonModule, HttpModule, SharedModule],
+    declarations: [AddActivityComponent],
     exports: [AddActivityComponent],
     providers: [AddActivityService]
 })
