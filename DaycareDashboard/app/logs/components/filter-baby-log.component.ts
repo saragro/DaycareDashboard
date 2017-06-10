c4cc6ea5867f15348f222572ef509fc19c8d6239
Sara Grossman <sgrossma5@gmail.com>
2017-06-11 01:55:51 +0300

    disable baby filter for parents

--- DaycareDashboard/app/logs/components/filter-baby-log.component.ts
+++ DaycareDashboard/app/logs/components/filter-baby-log.component.ts
@@ -1,72 +1,78 @@
 ﻿import { Component, OnInit, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { BabyLog, BabyLogService, Baby } from '../index';
-import { ActivatedRoute, Params ,Router} from "@angular/router";
-import { NgModel } from "@angular/forms";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
     selector: 'filter-baby-log',
     templateUrl: './app/logs/components/filter-baby-log.component.html',
     styleUrls: ['./app/logs/components/filter-baby-log.component.css'],
 })
-export class FilterBabyLogComponent implements OnInit{
+export class FilterBabyLogComponent implements OnInit {
     babyLog: BabyLog = new BabyLog();
     babies: Baby[] = [];
-	  
-	@ViewChild("myVar")
-    private selectViewModel: NgModel;
 
     constructor(private logService: BabyLogService, private route: ActivatedRoute, private router: Router) {
 
     }
 
     ngOnInit() {
-        let babyIdParam = this.route.snapshot.params["babyId"];
-        if (babyIdParam) {
-            this.initBabyLog(babyIdParam);
-        } else {
-            this.initBabyLog('1');
-        }
+        //params will change when a different baby is selected
+        this.route.params.subscribe(params => {
+            this.logService.getBabyList().toPromise().then(babies => {
+                babies.forEach(baby =>
+                    this.babies.push(new Baby(baby.id.trim(), baby.name.trim())));
+            }).then(() => {
+                let babyIdParam = this.route.snapshot.params["babyId"];
+                if (babyIdParam) {
+                    this.initBabyLog(babyIdParam);
+                } else {
+                    this.initBabyLog('1');
+                }
+            }
+           
+                );
+        });
 
-        this.logService.getBabyList().subscribe(babies => { 
-            babies.forEach(baby =>
-                this.babies.push(new Baby(baby.id, baby.name)));
-        }); 		 
+
     }
 
     onBabyChange(newBaby: Baby) {
         this.babyLog.baby = newBaby;
         let newBabyId = this.babyLog.baby.id;
-        this.filterByBabyId(newBabyId);
-        this.router.navigate(['log', { babyId: newBabyId}]);
+        //this.filterByBabyId(newBabyId);
+        this.router.navigate(['log', { babyId: newBabyId }]);
     }
 
-	 filterByBabyId(babyId: string) {
+    filterByBabyId(babyId: string) {
         this.logService.filterBabyLogByBabyId(babyId)
             .subscribe(baby => {
                 this.babyLog.date = new Date();
                 this.babyLog.actsLog = baby.actsLog;
                 console.log(this.babyLog);
             });
-      }
+    }
 
-	    initBabyLog(babyId: string) {
+    initBabyLog(babyId: string) {
         this.logService.filterBabyLogByBabyId(babyId)
             .subscribe(baby => {
                 this.babyLog = new BabyLog();
-                this.babyLog.baby = new Baby(baby.id, baby.name);
+                this.babyLog.baby = this.babies.find(b => b.id == babyId);
                 this.babyLog.date = new Date();
                 this.babyLog.actsLog = baby.actsLog;
                 console.log(this.babyLog);
             });
-        }
+    }
 
-	filterBabyLogByDate () {
-	  this.logService.filterBabyLogByDate(this.babyLog.baby.id,this.babyLog.date)
+    filterBabyLogByDate() {
+        this.logService.filterBabyLogByDate(this.babyLog.baby.id, this.babyLog.date)
             .subscribe(baby => {
                 this.babyLog.actsLog = baby.actsLog;
                 console.log(this.babyLog);
             });
-	}
+    }
 
+    isNanny() {
+        return sessionStorage.getItem('userRole') == 'nanny';
+    }
 
 }
